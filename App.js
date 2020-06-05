3972e285c264254073422159632c5cbf77271a15
Adrian <adrianbailador@hotmail.com>
2020-06-05 17:47:12 +0200

    Update App.js
    Creando la estructura del proyecto

--- App.js
+++ App.js
@@ -1,10 +1,30 @@
 import React from 'react';
 import {StyleSheet, View, Text, SafeAreaView} from 'react-native';
 
-export default function App(){
+export default function App() {
   return (
-    <SafeAreaView>
-      <Text>Hola Adri</Text>
-    </SafeAreaView>
-  )
+    <>
+      <SafeAreaView style={styles.SafeArea}>
+        <Text>Formulario HEAD</Text>
+      </SafeAreaView>
+
+      <View>
+        <Text>Resultado</Text>
+      </View>
+
+      <View>
+        <Text>Footer</Text>
+      </View>
+    </>
+  );
 }
+
+const styles = StyleSheet.create({
+  SafeArea: {
+    backgroundColor: '#f00',
+    height: 200,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    alignItems: 'center',
+  },
+});
