b531d7da0bbc7cde89c334b4f5a30cd7c3df9eaa
Adrian <adrianbailador@hotmail.com>
2020-06-15 18:06:10 +0200

    Logica para realizar el calculo

--- App.js
+++ App.js
@@ -18,11 +18,23 @@ export default function App() {
   const [capital, setCapital] = useState(null);
   const [interest, setInterest] = useState(null);
   const [months, setMonths] = useState(null);
+  const [total, setTotal] = useState(null);
 
   const calculate = () => {
-    console.log('capital ->', capital);
-    console.log('interest ->', interest);
-    console.log('months ->', months);
+    if(!capital) {
+      console.log('Añade la cantidad que quieres solicitar');
+    }else if (!interest) {
+      console.log('Añade el interes de los prestamos');
+    }else if(!months) {
+      console.log('Selecciona los meses a pagar');
+    }else {
+      const i = interest / 100;
+      const fee = capital / ((1 - Math.pow(i + 1, -months)) / i);
+      setTotal({
+        monthlyFee: fee.toFixed(2).replace('.', ','),
+        totalPayable: (fee * months).toFixed(2).replace('.', ','),
+      });
+    }
   };
 
   return (
